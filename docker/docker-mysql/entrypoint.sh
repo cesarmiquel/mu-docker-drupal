8629b551a6414642eb2d4cf4a508fc72de7212ab
Marcos Damián Mesmer y Rosset <marcos@easytech.com.ar>
2015-07-25 21:59:25 -0300

    Updates entrypint scritp for database creation

--- docker/docker-mysql/entrypoint.sh
+++ docker/docker-mysql/entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+set -e
 
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
+chown -R mysql:mysql /var/lib/mysql
+mysql_install_db --user mysql > /dev/null
 
-    mysql_install_db
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-""}
+MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_PASSWORD=${MYSQL_USER_PASSWORD:-""}
 
-    echo "CREATE DATABASE $db_name;" | mysql
-    echo "GRANT ALL ON $db_name TO $db_user@'%' IDENTIFIED BY '$db_pass' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-
-    killall mysqld
-    sleep 1s
+tfile=`mktemp`
+if [[ ! -f "$tfile" ]]; then
+    return 1
 fi
 
-mysqld
+cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
+EOF
+
+if [[ $MYSQL_DATABASE != "" ]]; then
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+    if [[ $MYSQL_USER != "" ]]; then
+        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+    fi
+fi
+
+/usr/bin/mysqld_safe --bootstrap --verbose=0 < $tfile
+rm -f $tfile
+
+exec /usr/bin/mysqld_safe
