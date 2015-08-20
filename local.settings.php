1e2f54242dafea427799703be1a4aa1edb725ed3
Cesar Miquel <miquel@easytech.com.ar>
2015-08-20 02:28:51 -0300

    Add local.settings.php needed to use Drupal

---
+++ local.settings.php
@@ -0,0 +1,20 @@
+<?php
+
+ini_set('display_error' , 1);
+ini_set('memory_limit', '500M');
+
+$databases = array (
+  'default' =>  
+  array (
+    'default' =>  
+    array (
+      'database' => 'drupal',
+      'username' => 'drupal',
+      'password' => 'drupal',
+      'host' => 'db',
+      'port' => '', 
+      'driver' => 'mysql',
+      'prefix' => '', 
+    ),  
+  ),  
+);
